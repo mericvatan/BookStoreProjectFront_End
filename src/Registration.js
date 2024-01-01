7c4a01a868b911176af0067a0c87e39752fbd880
mericvatan <mericvatan12@hotmail.com>
2024-01-01 17:28:22 +0300

    registration updated

--- src/Registration.js
+++ src/Registration.js
@@ -13,10 +13,16 @@ export default function Registration(){
 
     const handleSave = (e) => {
         e.preventDefault();
+
+        if (!namesurname || !email || !password || !confirmpassword || !userName) {
+          alert("Lütfen tüm alanları doldurun.");
+          return;
+      }
+
         const url = 'http://localhost:5045/api/User/CreateUser'
 
         const data = {
-            name: namesurname,
+            NameSurname: namesurname,
             email : email,
             password : password,
             confirmpassword : confirmpassword,
@@ -58,48 +64,48 @@ export default function Registration(){
     }
 
     return(
-  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style={{borderRadius: '15px'}}>
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Hesap Oluştur</h2>
+  <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div className="container h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="card" style={{borderRadius: '15px'}}>
+            <div className="card-body p-5">
+              <h2 className="text-uppercase text-center mb-5">Hesap Oluştur</h2>
 
               <form>
 
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" onChange={(e) => setNameSurname(e.target.value)} value={namesurname} />
-                  <label class="form-label" for="form3Example1cg">Ad-Soyad</label>
+                <div className="form-outline mb-4">
+                  <input type="text" id="form3Example1cg" className="form-control form-control-lg" onChange={(e) => setNameSurname(e.target.value)} value={namesurname} />
+                  <label className="form-label" htmlFor="form3Example1cg">Ad-Soyad</label>
                 </div>
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example2cg" class="form-control form-control-lg" onChange={(e) => setUsername(e.target.value)} value={userName} />
-                  <label class="form-label" for="form3Example1cg">Kullanıcı Adı</label>
+                <div className="form-outline mb-4">
+                  <input type="text" id="form3Example2cg" className="form-control form-control-lg" onChange={(e) => setUsername(e.target.value)} value={userName} />
+                  <label className="form-label" htmlFor="form3Example1cg">Kullanıcı Adı</label>
                 </div>
                
-                <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="email" id="form3Example3cg" className="form-control form-control-lg" 
                   onChange={(e) => setEmail(e.target.value)} value={email} />
-                  <label class="form-label" for="form3Example3cg">E-posta</label>
+                  <label className="form-label" htmlFor="form3Example3cg">E-posta</label>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cg" className="form-control form-control-lg" 
                   onChange={(e) => setPassword(e.target.value)} value={password} />
-                  <label class="form-label" for="form3Example4cg">Şifre</label>
+                  <label className="form-label" htmlFor="form3Example4cg">Şifre</label>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cdg" className="form-control form-control-lg" 
                   onChange={(e) => setConfirmPassword(e.target.value)} value={confirmpassword}/>
-                  <label class="form-label" for="form3Example4cdg">Şifre tekrar</label>
+                  <label className="form-label" htmlFor="form3Example4cdg">Şifre tekrar</label>
                 </div>
 
             
 
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <button type="button"
-                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" onClick={(e) => handleSave(e)}>Kaydol</button>
+                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body" onClick={(e) => handleSave(e)}>Kaydol</button>
                 </div>
 
                 <p className="text-center text-muted mt-5 mb-0">Zaten hesabın var mı? <a href="#!" className="fw-bold text-body" onClick={(e) => handleLogin(e)}><u>Giriş Yap</u></a></p>
