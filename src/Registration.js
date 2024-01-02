f8e1e1a76fec1dde3bffc1ae143a8d6c5d5adef3
mericvatan <mericvatan12@hotmail.com>
2024-01-02 19:36:21 +0300

    regis tekrar düzenlendi

--- src/Registration.js
+++ src/Registration.js
@@ -9,16 +9,19 @@ export default function Registration(){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmpassword, setConfirmPassword] = useState('')
+    const [userName, setUsername] = useState('')
+
 
     const handleSave = (e) => {
         e.preventDefault();
         const url = 'http://localhost:5045/api/User/CreateUser'
 
         const data = {
-            name: namesurname,
+            NameSurname: namesurname,
             email : email,
             password : password,
-            confirmpassword : confirmpassword
+            confirmpassword : confirmpassword,
+            userName:userName
         }
 
         axios.post(url, data)
@@ -29,7 +32,18 @@ export default function Registration(){
             
         })
         .catch((error) =>{
-            console.log(error);
+          if (error.response) {
+            // Sunucu tarafından dönen hata ayrıntıları
+            console.log("Error response from server:", error.response.data);
+            console.log("Status code:", error.response.status);
+            console.log("Headers:", error.response.headers); 
+        } else if (error.request) {
+            // İstek yapıldı, ancak hiçbir yanıt alınmadı
+            console.log("No response received:", error.request);
+        } else {
+            // İstek yapılırken bir hata oluştu
+            console.log("Error during request setup:", error.message);
+        }
         })
     }
 
@@ -45,42 +59,46 @@ export default function Registration(){
     }
 
     return(
-  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style={{borderRadius: '15px'}}>
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Hesap Oluştur</h2>
+  <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div className="container h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="card" style={{borderRadius: '15px'}}>
+            <div className="card-body p-5">
+              <h2 className="text-uppercase text-center mb-5">Hesap Oluştur</h2>
 
               <form>
 
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" onChange={(e) => setNameSurname(e.target.value)} value={namesurname} />
-                  <label class="form-label" for="form3Example1cg">Ad-Soyad</label>
+                <div className="form-outline mb-4">
+                  <input type="text" id="form3Example1cg" className="form-control form-control-lg" onChange={(e) => setNameSurname(e.target.value)} value={namesurname} />
+                  <label className="form-label" htmlFor="form3Example1cg">Ad-Soyad</label>
+                </div>
+                <div className="form-outline mb-4">
+                  <input type="text" id="form3Example2cg" className="form-control form-control-lg" onChange={(e) => setUsername(e.target.value)} value={userName} />
+                  <label className="form-label" htmlFor="form3Example1cg">Kullanıcı Adı</label>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="email" id="form3Example3cg" className="form-control form-control-lg" 
                   onChange={(e) => setEmail(e.target.value)} value={email} />
-                  <label class="form-label" for="form3Example3cg">E-posta</label>
+                  <label className="form-label" htmlFor="form3Example3cg">E-posta</label>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cg" className="form-control form-control-lg" 
                   onChange={(e) => setPassword(e.target.value)} value={password} />
-                  <label class="form-label" for="form3Example4cg">Şifre</label>
+                  <label className="form-label" htmlFor="form3Example4cg">Şifre</label>
                 </div>
 
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" 
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4cdg" className="form-control form-control-lg" 
                   onChange={(e) => setConfirmPassword(e.target.value)} value={confirmpassword}/>
-                  <label class="form-label" for="form3Example4cdg">Şifre tekrar</label>
+                  <label className="form-label" htmlFor="form3Example4cdg">Şifre tekrar</label>
                 </div>
 
-                <div class="d-flex justify-content-center">
+                <div className="d-flex justify-content-center">
                   <button type="button"
-                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body" onClick={(e) => handleSave(e)}>Kaydol</button>
+                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body" onClick={(e) => handleSave(e)}>Kaydol</button>
                 </div>
 
                 <p className="text-center text-muted mt-5 mb-0">Zaten hesabın var mı? <a href="#!" className="fw-bold text-body" onClick={(e) => handleLogin(e)}><u>Giriş Yap</u></a></p>
