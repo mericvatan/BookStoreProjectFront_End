0f117dc97e339e67bceaef5f3bfb9e3d147cc115
mericvatan <mericvatan12@hotmail.com>
2024-01-11 00:54:14 +0300

    bazı düzenlemeler yapıldı.

--- src/Products.js
+++ src/Products.js
@@ -8,7 +8,7 @@ function Products() {
     // Backend'ten ürünleri getir
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:5045/api/Products'); // Örnek bir endpoint
+        const response = await axios.get('http://localhost:5045/api/Product/All'); // Örnek bir endpoint
         setProducts(response.data);
       } catch (error) {
         console.error('Ürünleri getirirken bir hata oluştu:', error);
@@ -25,11 +25,10 @@ function Products() {
         {products.map((product) => (
           <div key={product.id} className="col-md-4 mb-4">
             <div className="card">
-              <img src={product.image} className="card-img-top" alt={product.name} />
+
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.description}</p>
-                <p className="card-text">${product.price}</p>
+                <p className="card-text">{product.price}</p>
                 {/* Diğer ürün bilgileri */}
               </div>
             </div>
