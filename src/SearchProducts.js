da9ff8363edfb7635fd70b037d8c7de7b6a9c73c
mericvatan <mericvatan12@hotmail.com>
2024-01-21 19:52:44 +0300

    updated SearchProduct.js

--- src/SearchProducts.js
+++ src/SearchProducts.js
@@ -7,19 +7,18 @@ function SearchProducts({ product }) {
   return (
     <div>
       <div className="row">
-        <div key={product.id} className="col-md-3 mb-2">
-          <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
-            <div className="card" style={{ backgroundColor: 'rgba(128, 128, 128, 0.1)' }}>
-              <div className="card-body">
-                <img src={`data:image/jpeg;base64, ${product.imageUrl}`} width={100} height={190} alt={product.name} />
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.price} TL</p>
-              </div>
+        <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
+          <div className="card" style={{ backgroundColor: 'rgba(128, 128, 128, 0.1)' }}> 
+            <div className="card-body"> 
+              <img src={`data:image/jpeg;base64, ${product.imageUrl}`} width={100} height={190} />
+              <h5 className="card-title">{product.name}</h5>
+              <p className="card-text">{product.price} TL</p>
             </div>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </div>
+      
   );
 }
 
