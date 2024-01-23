1b12298f63f963a711c422431cc3652f4873672b
Özge Kurnaz <ozge.k60@hotmail.com>
2024-01-23 21:47:59 +0300

    ProductDetails.js Updated

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Padding } from "@mui/icons-material";
+import { width } from "@mui/system";
 
 function ProductDetails() {
   const { id } = useParams();
@@ -9,8 +11,10 @@ function ProductDetails() {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`http://localhost:5045/api/Product/GetProductById/${id}`);
-        setProduct(response.data);  
+        const response = await axios.get(
+          `http://localhost:5045/api/Product/GetProductById/${id}`
+        );
+        setProduct(response.data);
       } catch (error) {
         console.error("Ürünleri getirirken bir hata oluştu:", error);
       }
@@ -24,17 +28,47 @@ function ProductDetails() {
   }
 
   return (
-    <div>
-      <h2 className="text-uppercase text-center mb-5">Kitap Detayları</h2>
-      <div className="card" style={{ backgroundColor: 'rgba(128, 128, 128, 0.1)' }}>
-        <div className="card-body">
-          <img src={`data:image/jpeg;base64, ${product.imageUrl}`} width={100} height={190} alt={product.name} />
-          <h5 className="card-title">{product.name}</h5>
-          <p className="card-text">{product.price} TL</p>
+    <div className="justify-content-center" style={{ paddingTop:"9rem"}}   >
+    <div className="container d-flex justify-content-center align-items-center" style={{ backgroundColor: 'rgba(128, 128, 128, 0.1)' }}>
+      <div className="col-md-4" >
+        <img
+          src={`data:image/jpeg;base64, ${product.imageUrl}`}
+          alt={product.name}
+          style={{ paddingTop:"0rem"}}
+        />
+      </div> 
+      <div className="col-md-5">
+        <div className="row-md-5">
+          <div
+            className="text-start"
+            style={{ fontSize: "16px",paddingTop:"5rem"}}
+          >
+            <h5 style={{ fontSize: "30px", fontWeight: "bold" }}>
+              {product.name}
+            </h5>
+
+            <p style={{ fontSize: "20px" }}>{product.authorName}</p>
+            <p>
+              <a>Fiyat:</a>
+              {product.price} TL
+            </p>
+            <p>
+              <a>Sayfa Sayısı:</a>
+              {product.pages}
+            </p>
+            </div>
+            <p className="text-start" >
+              <a style={{ fontSize: "16px", fontWeight: "bolder" }}>AÇIKLAMA</a><br />
+              {product.description}
+            </p>
+         
         </div>
       </div>
-    </div>      
+    </div>
+  </div>
+  
   );
 }
 
 export default ProductDetails;
+
